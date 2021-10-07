bc824480b940869186538a655178cd401320d562
547841220 <18935114910@163.com>
2021-10-07 17:12:37 +0800

    2021.10.07 Hystrix dashboard图形化界面搭建

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/jijie/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.jijie.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author jijie
+ * @Date 2021/5/17 11:11
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
